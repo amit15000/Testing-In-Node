61448119b67b99f38fbf395a6df1fcc9c78b9bc5
Amit Kumar <15000amitkumar@gmail.com>
2025-05-12 00:18:13 +0530

    Db mocking

--- testExpress/src/tests/index.test.js
+++ testExpress/src/tests/index.test.js
@@ -1,14 +1,25 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, it, vi } from "vitest";
 import request from "supertest";
 import { app } from "../index.js";
+
+vi.mock("../db.js", () => {
+  return {
+    prismaClient: {
+      Sum: {
+        create: vi.fn(),
+      },
+    },
+  };
+});
+
 describe("POST  /sum", () => {
   it("should return the sum of two numbers", async () => {
     const res = await request(app).post("/sum").send({
-      a: 1,
-      b: 2,
+      a: 4,
+      b: 5,
     });
     console.log(res);
     expect(res.statusCode).toBe(200);
-    expect(res.body.answer).toBe(3);
+    expect(res.body.answer).toBe(9);
   });
 });
